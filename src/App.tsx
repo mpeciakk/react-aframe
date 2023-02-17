47f7da98c187ce3afdea56c68856a8a6c61bf623
mvciek <mpeciakk@gmail.com>
2023-02-17 14:03:51 +0100

    react strict mode

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,9 @@ import {
   VideoSphere
 } from '@belivvr/aframe-react'
 import pano from "./assets/pano.mp4"
+import play from "./assets/play.png"
+import pause from "./assets/pause.png"
+import { useState } from "react"
 
 AFRAME.registerComponent("click", {
   schema: {
@@ -18,12 +21,14 @@ AFRAME.registerComponent("click", {
     const { cmd } = this.data
 
     this.el.addEventListener("click", () => {
+      const video = document.getElementById("pano") as HTMLVideoElement
+
       if (cmd === "play") {
-        document.getElementById("pano").play()
+        video.play()
       }
 
       if (cmd === "stop") {
-        document.getElementById("pano").pause()
+        video.pause()
       }
     })
   }
@@ -31,35 +36,63 @@ AFRAME.registerComponent("click", {
 
 
 function App() {
+  const [url, setUrl] = useState<string>(`${pano}#t=0.1`)
+
   return (
     <div>
       <Scene>
         <Assets>
           <img id="cubes" src="https://cdn.aframe.io/360-image-gallery-boilerplate/img/thumb-cubes.jpg" />
-          <video id="pano" src={`${pano}#t=0.1`} loop />
+          <img id="play" src={play} />
+          <img id="pause" src={pause} />
+          <video id="pano" src={url} loop />
         </Assets>
 
-        <Box
-          position={{ x: 1, y: 0, z: -3 }}
-          rotation={{ x: 0, y: 0, z: 0 }}
-          color="#4CC3D9"
+        <Entity
+          geometry={{ primitive: "plane", height: 1, width: 1 }}
+          material={{ shader: "flat", src: "#pause", transparent: true }}
+          position={{ x: 0.75, y: 0, z: -3 }}
+          rotation={{ x: -30, y: 0, z: 0 }}
           click={{ cmd: "stop" }}
           data-clickable
         />
 
-        <Box
-          position={{ x: -1, y: 0, z: -3 }}
-          rotation={{ x: 0, y: 0, z: 0 }}
-          color="#4CC3D9"
+
+        <Entity
+          geometry={{ primitive: "plane", height: 1, width: 1 }}
+          material={{ shader: "flat", src: "#play", transparent: true }}
+          position={{ x: -0.75, y: 0, z: -3 }}
+          rotation={{ x: -30, y: 0, z: 0 }}
           click={{ cmd: "play" }}
           data-clickable
+        />
+
+        <Box
+          position={{ x: 0, y: -0.3, z: -3.5 }}
+          rotation={{ x: -30, y: 0, z: 0 }}
+          geometry={{ width: 3, height: 1 }}
+          color="#4CC3D9"
+        />
+
+        <Box
+          position={{ x: -1.7, y: -0.95, z: -3.5 }}
+          rotation={{ x: -30, y: 0, z: 60 }}
+          geometry={{ width: 2, height: 1 }}
+          color="#4CC3D9"
+        />
+
+        <Box
+          position={{ x: 1.7, y: -0.95, z: -3.5 }}
+          rotation={{ x: -30, y: 0, z: -60 }}
+          geometry={{ width: 2, height: 1 }}
+          color="#4CC3D9"
         />
 
         <Camera>
           <Entity
             position={{ x: 0, y: 0, z: -1 }}
             geometry={{ primitive: "ring", radiusInner: 0.02, radiusOuter: 0.03 }}
-            material={{ color: "black", shader: "flat" }}
+            material={{ color: "red", shader: "flat" }}
             cursor={{ fuse: true }}
             animation__click={{ property: "scale", startEvents: ["click"], easing: "easeInCubic", dur: 150, from: "0.1 0.1 0.1", to: "1 1 1" }}
             animation__fusing={{ property: "scale", startEvents: ["fusing"], easing: "easeInCubic", dur: 1500, from: "1 1 1", to: "0.1 0.1 0.1" }}
